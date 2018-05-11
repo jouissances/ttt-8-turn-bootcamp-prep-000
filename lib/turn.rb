f1b58b0965af31df68d1f082325763e4068e237f
Janice Darikho <janicedarikho@gmail.com>
2018-05-11 13:34:33 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,7 +7,10 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
- 
+def input_to_index(input)
+    input.to_i - 1
+end
+  
 
 def move(board, index, value = "X")
     board[index] = value
@@ -33,13 +36,8 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  
-  def input_to_index(input)
-    input.to_i - 1
-  end
-  
+
+
   if valid_move?(board, index) 
     return true
   else 
